0aa815fcb5e9235b5bae225313acf4780a8883ea
Igor <houwat@gmail.com>
2021-01-08 17:30:08 -0500

    Add Inner static singleton

--- src/com/creational/singleton/LazySingleton.java
+++ src/com/creational/singleton/LazySingleton.java
@@ -1,4 +1,4 @@
-// Lazy singleton and thread safety
+// Lazy singleton and thread safety, inner static singleton
 
 package creational.singleton;
 
@@ -11,7 +11,7 @@ class LazySingleton
         System.out.println("initializing a lazy singleton");
     }
 
-    // synchronized for thread-safety - performance is an issue
+    // 1st approach: synchronized for thread-safety - performance is an issue
 //    public static synchronized LazySingleton getInstance()
 //    {
 //        // Checks if it has been created on lazy loading
@@ -22,19 +22,39 @@ class LazySingleton
 //        return instance;
 //    }
 
-    // double-checked locking: check the instance loading twice
-    public static LazySingleton getInstance()
-    {
-        if (instance == null)
-        {
-            synchronized (LazySingleton.class)
-            {
-                if (instance == null)
-                {
-                    instance = new LazySingleton();
-                }
-            }
-        }
-        return instance;
-    }
+    // 2nd approach: double-checked locking: check the instance loading twice
+//    public static LazySingleton getInstance()
+//    {
+//        if (instance == null)
+//        {
+//            synchronized (LazySingleton.class)
+//            {
+//                if (instance == null)
+//                {
+//                    instance = new LazySingleton();
+//                }
+//            }
+//        }
+//        return instance;
+//    }
 }
+
+//    3rd approach: inner static class
+
+    class InnerStaticSingleton
+    {
+        private InnerStaticSingleton(){}
+        // inner class here
+        private static class Impl
+        {
+            private static final InnerStaticSingleton
+                INSTANCE = new InnerStaticSingleton(); // inner classes can access private members of outer classes
+        }
+
+        //Expose instance - this approach avoids the problem of synchronization
+        public InnerStaticSingleton getInstance()
+        {
+            return Impl.INSTANCE;
+        }
+    }
+
