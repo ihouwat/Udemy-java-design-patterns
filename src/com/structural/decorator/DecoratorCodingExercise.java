87ec46648d4fab838049234b31580dbe37256ab3
Igor <houwat@gmail.com>
2021-01-10 12:45:25 -0500

    Added DecoratorCodingExercise.java

---
+++ src/com/structural/decorator/DecoratorCodingExercise.java
@@ -0,0 +1,60 @@
+/*
+Exercise for decorator section
+Complete the Dragon class, which is BOTH a Lizard and a Bird
+ */
+
+package structural.decorator;
+
+class Bird
+{
+    public int age;
+
+    public String fly()
+    {
+        return age < 10 ? "flying" : "too old";
+    }
+}
+
+class Lizard
+{
+    public int age;
+
+    public String crawl()
+    {
+        return (age > 1) ? "crawling" : "too young";
+    }
+}
+
+class Dragon {
+    private int age;
+    // Solution: instead of inheriting or creating a common interface, you instantiate two classes in the Dragon class
+    private Bird bird = new Bird();
+    private Lizard lizard = new Lizard();
+
+    // Need empty cons
+    public Dragon() {}
+
+    public void setAge(int age)
+    {
+        // Set age of all animals to be the same
+        this.age = bird.age = lizard.age = age;
+    }
+    public String fly()
+    {
+        return bird.fly();
+    }
+    public String crawl()
+    {
+        return lizard.crawl();
+    }
+}
+
+class DecoratorExerciseDemo
+{
+    public static void main(String[] args) {
+        Dragon dragon = new Dragon();
+        dragon.setAge(0);
+        System.out.println(dragon.crawl());
+        System.out.println(dragon.fly());
+    }
+}
